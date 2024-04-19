e5ef4233ebf36db78ccc60354c523d72f61b5f67
cmaul25 <167263885+cmaul25@users.noreply.github.com>
2024-04-19 12:48:05 -0500

    Init power mute

---
+++ television.py
@@ -0,0 +1,24 @@
+
+class Television():
+    MIN_VOLUME=0
+    MAX_VOLUME=2
+    MIN_CHANNEL=0
+    MAX_CHANNEL=3
+
+    def __init__(self):
+        self.status=False
+        self.muted=False
+        self.volume=Television.MIN_VOLUME
+        self.channel=Television.MIN_CHANNEL
+
+    def power(self):
+        if self.status==False:
+            self.status=True
+        else:
+            self.status=False
+
+    def mute(self):
+        if self.muted==False:
+            self.muted=True
+        else:
+            self.muted=False
