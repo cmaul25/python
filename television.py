33ba4373e83bde4641ee2bec30c9d6aa8b83ed17
cmaul25 <167263885+cmaul25@users.noreply.github.com>
2024-04-22 19:16:46 -0500

    private vars

--- television.py
+++ television.py
@@ -9,70 +9,70 @@ class Television():
         '''
         Sets up status, muted, volume, and channel instance variables
         '''
-        self.status=False
-        self.muted=False
-        self.volume=Television.MIN_VOLUME
-        self.channel=Television.MIN_CHANNEL
+        self.__status=False
+        self.__muted=False
+        self.__volume=Television.MIN_VOLUME
+        self.__channel=Television.MIN_CHANNEL
 
     def power(self)->None:
         '''
         Switches power on and off
         '''
-        if self.status==False:
-            self.status=True
+        if self.__status==False:
+            self.__status=True
         else:
-            self.status=False
+            self.__status=False
 
     def mute(self)->None:
         '''
         Switches mute on and off
         '''
-        if self.status == True:
-            if self.muted==False:
-                self.muted=True
+        if self.__status == True:
+            if self.__muted==False:
+                self.__muted=True
             else:
-                self.muted=False
+                self.__muted=False
 
     def channel_up(self)->None:
         '''
         Increments channel up by one unless at max channel then sets to channel minimum
         '''
-        if self.status==True:
-            if self.channel!=Television.MAX_CHANNEL:
-                self.channel+=1
+        if self.__status==True:
+            if self.__channel!=Television.MAX_CHANNEL:
+                self.__channel+=1
             else:
-                self.channel=Television.MIN_CHANNEL
+                self.__channel=Television.MIN_CHANNEL
 
     def channel_down(self)->None:
         '''
         Increments channel by -1 unless at channel min then set channel to channel max
         :return:
         '''
-        if self.status == True:
-            if self.channel!=Television.MIN_CHANNEL:
-                self.channel-=1
+        if self.__status == True:
+            if self.__channel!=Television.MIN_CHANNEL:
+                self.__channel-=1
             else:
-                self.channel=Television.MAX_CHANNEL
+                self.__channel=Television.MAX_CHANNEL
 
     def volume_up(self)->None:
         '''
         unmutes tv and increases volume by one unless at volume max
         :return:
         '''
-        if self.status == True:
-            self.muted=False
-            if self.volume!=Television.MAX_VOLUME:
-                self.volume+=1
+        if self.__status == True:
+            self.__muted=False
+            if self.__volume!=Television.MAX_VOLUME:
+                self.__volume+=1
 
     def volume_down(self)->None:
         '''
         unmutes tv and decreases volume by one unless at volume min
         :return:
         '''
-        if self.status == True:
-            self.muted = False
-            if self.volume!=Television.MIN_VOLUME:
-                self.volume-=1
+        if self.__status == True:
+            self.__muted = False
+            if self.__volume!=Television.MIN_VOLUME:
+                self.__volume-=1
 
     def __str__(self)->str:
         '''
@@ -80,8 +80,8 @@ class Television():
                     Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}.
         :return:
         '''
-        if self.muted==False:
-            return f'Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}.'
+        if self.__muted==False:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}.'
         else:
-            return f'Power = {self.status}, Channel = {self.channel}, Volume = {Television.MIN_VOLUME}.'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}.'
 
